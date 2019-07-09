3e3343d20cf70473320941ce4ebe72580e07c457
Nicolo Tartaggia <nicolo.tartaggia@uqido.com>
2019-07-09 23:50:53 +0200

    [AUTH] Added log out and some style

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,19 +5,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatListModule,
     MatExpansionModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     MatListModule,
     MatExpansionModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 
